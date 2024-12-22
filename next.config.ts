6682bde9f6573eea57b32341ef64265723f0a351
tanvishdesai <vikramdesai434@gmail.com>
2024-12-22 18:57:10 +0530

    for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // This ensures the URLs have trailing slashes, which GitHub Pages requires
-  output: 'export',
-  trailingSlash: true,
-  basePath: '/messaging-board',
-  assetPrefix: '/messaging-board',
-  // Enable static export
-  // This tells Next.js that you want to export your site as static HTML 
-
-  // Additional custom configurations (optional)
-};
-
-export default nextConfig;
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+}
+ 
+module.exports = nextConfig
