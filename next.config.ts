a377d51908b32482b5c3e95c915c6d3821a453d3
tanvishdesai <vikramdesai434@gmail.com>
2024-12-22 18:38:27 +0530

    out taken out

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // This ensures the URLs have trailing slashes, which GitHub Pages requires
-  // output: 'export',
-  // trailingSlash: true,
-  // basePath: '/messaging-board',
-  // assetPrefix: '/messaging-board',
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/messaging-board',
+  assetPrefix: '/messaging-board',
   // Enable static export
   // This tells Next.js that you want to export your site as static HTML 
 
