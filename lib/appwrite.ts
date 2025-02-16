8c234bd67ed7da09f6e47a716842382e34531fc7
tanvishdesai <vikramdesai434@gmail.com>
2025-02-17 00:04:54 +0530

    vercel auth v3

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -40,7 +40,7 @@ export async function getLoggedInUser() {
   try {
     const { account } = await createSessionClient();
     return await account.get();
-  } catch (error) {
+  } catch {
     return null;
   }
 }
